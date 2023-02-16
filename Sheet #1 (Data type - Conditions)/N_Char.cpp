c8330e126868b7a0ee41f3ff1809ae8eb394d3d2
Shahriar Mahmud Arman <iamarman252575@gmail.com>
2023-02-16 13:04:24 +0600

    Add files via upload

---
+++ Sheet #1 (Data type - Conditions)/N_Char.cpp
@@ -0,0 +1,84 @@
+                                          //   Bismillahir Rahmanir Rahim      //
+                                         //      SHAHRIAR MAHMUD ARMAN        //
+                                        //         AUTHOR : iamarman         //
+
+
+#include<bits/stdc++.h>
+
+
+                                                  //// MACROS ////
+
+//---------------------------------------------------------------------------------------------------------------------------------|
+
+
+# define ll long long                                                                                                              
+# define test int test; cin>>test; while(test--)
+# define rep(i,a,b) for(int i=a;i<b;i++)
+# define rep_n(i,a,b) for(int i=a;i<=b;i++)
+# define rep_rev(i,a,b) for(int i=b;a>=1;i--)
+# define pb push_back
+# define el "\n"
+# define optimise   ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); cin.clear(); 
+# define R0 return 0
+# define all(v) v.begin(),v.end()
+# define srt(vvv) sort(vvv.begin(),vvv.end())
+# define sp " "
+# define vii vector<int>
+# define vll vector<long long>
+# define yess cout<<"YES"<<el
+# define noo cout<<"NO"<<el
+# define PI 2*acos(0.0)
+ll mod=10000000+7;
+
+
+//----------------------------------------------------------------------------------------------------------------------------------|
+                                             ///      NAMESPACE     ///
+
+using namespace std;
+
+
+                    /// THIS IS FOR GIVING INPUT AND GETTING OUTPUT IN A FILE INSTEAD OF TERMINAL OR CONSOLE   ///
+
+
+inline void file() {
+
+#ifndef ONLINE_JUDGE
+
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+
+#endif // ONLINE_JUDGE
+}
+
+                                                    ///  CODE STARTS FROM HERE    ///
+
+
+
+
+void solve()
+{
+   char c;
+   cin>>c;
+
+   if(islower(c))
+   {
+    cout<<(char)toupper(c)<<endl;
+   }
+   else if(isupper(c))
+   {
+    cout<<(char)tolower(c)<<endl;
+   }
+
+
+
+}
+
+int main()
+{
+    optimise;
+    file();
+    solve();
+
+}
+
+//// PROBLEM LINK : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/N
